b69f09316206ab95bd6e3bfaba63613be692d0a7
Tanner Krewson <tannerkrewson@gmail.com>
2020-07-09 13:09:43 -0500

    add logo to page

---
+++ components/Footer.tsx
@@ -0,0 +1,27 @@
+const Footer = () => (
+    <footer>
+        rocketcrab by{" "}
+        <a
+            href="https://www.tannerkrewson.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            Tanner Krewson
+        </a>
+        <br />
+        <a
+            href="https://github.com/tannerkrewson/rocketcrab"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            View on GitHub
+        </a>
+        <style jsx>{`
+            font-size: 12px;
+            text-align: center;
+            margin-bottom: 2em;
+        `}</style>
+    </footer>
+);
+
+export default Footer;
