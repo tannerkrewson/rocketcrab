a86deae80ffe1077b0f568354003a16735f1ccb4
Tanner Krewson <tannerkrewson@gmail.com>
2020-07-12 17:11:52 -0500

    fixed game list not working in prod mode

--- games/index.ts
+++ games/index.ts
@@ -3,11 +3,7 @@ import fs from "fs";
 export default () => {
     const gameList = [];
     fs.readdirSync(__dirname).forEach((file) => {
-        if (
-            file === "index.ts" ||
-            file.substr(file.lastIndexOf(".") + 1) !== "ts"
-        )
-            return;
+        if (file.startsWith("index.")) return;
         const name = file.substr(0, file.indexOf("."));
         gameList.push(require("./" + name).default);
     });
