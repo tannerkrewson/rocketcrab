73f8725b99da03181af9ba5cd8fc5d8e2d1e8aac
Tanner Krewson <tannerkrewson@gmail.com>
2020-07-12 22:10:45 -0500

    collapse status bar on logo tap

--- components/templates/GameLayout.tsx
+++ components/templates/GameLayout.tsx
@@ -1,18 +1,31 @@
 import { GameStatus } from "../../types/enums";
 import { Loading } from "@zeit-ui/react";
 import PrimaryButton from "../atoms/PrimaryButton";
+import { useState } from "react";
 
 const GameLayout = ({ gameState, onExitGame }) => {
     const { status, url } = gameState;
+
+    const [statusCollapsed, setStatusCollapsed] = useState(false);
+
     const showLoading = status === GameStatus.loading;
     const showGameFrame = status === GameStatus.inprogress;
+
+    const statusClass = "status " + (statusCollapsed ? "status-collapsed" : "");
     return (
         <div className="layout">
-            <div className="status">
-                <h4>🚀🦀</h4>
-                <PrimaryButton onClick={onExitGame} size="small">
-                    Back to Lobby
-                </PrimaryButton>
+            <div className={statusClass}>
+                <h4
+                    className="logo"
+                    onClick={() => setStatusCollapsed(!statusCollapsed)}
+                >
+                    🚀🦀
+                </h4>
+                {!statusCollapsed && (
+                    <PrimaryButton onClick={onExitGame} size="small">
+                        Back to Lobby
+                    </PrimaryButton>
+                )}
             </div>
             {showLoading && (
                 <div className="frame">
@@ -32,12 +45,20 @@ const GameLayout = ({ gameState, onExitGame }) => {
                     justify-content: space-between;
                     padding: 0.5em;
                 }
+                .status-collapsed {
+                    position: fixed;
+                    width: fit-content;
+                    border-right: 1px solid LightGrey;
+                }
+
                 .frame {
                     flex: 1 1 auto;
                     border: 0;
                 }
-                h4 {
+                .logo {
                     margin: 0;
+                    user-select: none;
+                    cursor: pointer;
                 }
             `}</style>
         </div>
