4fc34a71b09b87835093fdefa07bbdac3ec7cb66
tannerkrewson <tannerkrewson@gmail.com>
2020-09-23 21:53:18 -0500

    added manifest and icons

--- components/templates/Body.tsx
+++ components/templates/Body.tsx
@@ -8,6 +8,31 @@ const Body = ({ children }: BodyProps): JSX.Element => (
                 name="viewport"
                 content="width=device-width, initial-scale=1"
             />
+            <link
+                rel="apple-touch-icon"
+                sizes="180x180"
+                href="/apple-touch-icon.png"
+            />
+            <link
+                rel="icon"
+                type="image/png"
+                sizes="32x32"
+                href="/favicon-32x32.png"
+            />
+            <link
+                rel="icon"
+                type="image/png"
+                sizes="16x16"
+                href="/favicon-16x16.png"
+            />
+            <link rel="manifest" href="/manifest.webmanifest" />
+            <link
+                rel="mask-icon"
+                href="/safari-pinned-tab.svg"
+                color="#000000"
+            />
+            <meta name="msapplication-TileColor" content="#ffffff" />
+            <meta name="theme-color" content="#ffffff" />
         </Head>
         {children}
         <style jsx global>{`
