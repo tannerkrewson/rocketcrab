f7b8c43d0a2afeda5a8c29a742111ebaf0a95ccf
tannerkrewson <tannerkrewson@gmail.com>
2020-09-24 22:07:51 -0500

    add rocketcrab as app button

--- test/pages/index.test.tsx
+++ test/pages/index.test.tsx
@@ -2,6 +2,20 @@ import React from "react";
 import { render } from "../testUtils";
 import { Home } from "../../pages/index";
 
+Object.defineProperty(window, "matchMedia", {
+    writable: true,
+    value: jest.fn().mockImplementation((query) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // deprecated
+        removeListener: jest.fn(), // deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+    })),
+});
+
 describe("pages/index.tsx", () => {
     it("matches snapshot", () => {
         const { asFragment } = render(<Home />, {});
