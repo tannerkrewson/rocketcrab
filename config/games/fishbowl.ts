0143e64cd167340ac59b924efb354a9ddc5104d1
tannerkrewson <tannerkrewson@gmail.com>
2020-11-03 20:42:09 -0600

    added fishbowl :fish:

---
+++ config/games/fishbowl.ts
@@ -0,0 +1,47 @@
+import { ServerGame } from "../../types/types";
+import { postJson } from "../../utils/utils";
+
+const game: ServerGame = {
+    id: "fishbowl",
+    name: "Fishbowl",
+    author: "Avi Moondra",
+    description:
+        "Fishbowl is a virtual version of a fun (and mostly hilarious) guessing game, designed for any group of all ages! You'll need at least 4 to play, but it only gets more fun with more players. Hop on a video call, and play through rounds of Taboo, Charades, and Password.",
+    displayUrlText: "fishbowl-game.com",
+    displayUrlHref: "https://fishbowl-game.com/",
+    donationUrlText: "Buy Avi a coffee!",
+    donationUrlHref: "https://www.buymeacoffee.com/fishbowlgame",
+    guideId: "fishbowl",
+    pictures: [
+        "https://i.imgur.com/bWlUX5R.jpg",
+        "https://i.imgur.com/YkEzh3q.jpg",
+        "https://i.imgur.com/Kj5ZZyV.jpg",
+        "https://i.imgur.com/NZB2fGn.jpg",
+        "https://i.imgur.com/EgXpK11.jpg",
+        "https://i.imgur.com/eY5NF46.jpg",
+        "https://i.imgur.com/OtRqoy5.jpg",
+        "https://i.imgur.com/DPCalID.jpg",
+    ],
+    category: ["easy"],
+    players: "4+",
+    familyFriendly: true,
+    getJoinGameUrl: async () => {
+        const newGameUrl = "https://fishbowl-graphql.onrender.com/v1/graphql";
+        const {
+            data: {
+                newGame: { join_code },
+            },
+        } = await postJson(newGameUrl, {
+            query: "mutation  {\n  newGame {\n    join_code\n  }\n}\n",
+            variables: null,
+        });
+        return {
+            playerURL: "https://fishbowl-game.com/game/" + join_code + "/lobby",
+        };
+    },
+    renameParams: {
+        rocketcrab: "hideshare",
+    },
+};
+
+export default game;
---
+++ config/guides/fishbowl.md
@@ -0,0 +1,9 @@
+First, everyone writes down a few words or phrases on "cards" that will be guessed later. The group will then randomly be split into two teams!
+
+Players from each team will alternate turns, giving clues to their team to guess as many cards as possible against the clock. There'll be 3 rounds, and cards will get recycled after each round!
+
+-   Round 1: Taboo. Use words to describe the word or phrase on the card, without any acting or gestures. You cannot use any part of the word or phrase!
+-   Round 2: Charades. Without words or sounds, act and use gestures to communicate the word or phrase on the card.
+-   Round 3: Password. You can say exactly one word to describe the word or phrase on the card, no more! You'll rely on your team's memory and association.
+
+The team that guesses the most cards across all rounds wins!
