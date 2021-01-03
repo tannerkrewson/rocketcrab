94fad544409c2fc8475691a4e5640e90269fc78a
fravic <fravicf@gmail.com>
2021-01-02 17:04:47 -0800

    Add guide and image

--- config/games/4inarow.ts
+++ config/games/4inarow.ts
@@ -8,7 +8,8 @@ const game: ServerGame = {
     basedOn: {
         game: "Connect Four",
     },
-    description: "Play a quick game of 4 in a Row with a friend, in seconds",
+    description:
+        "A quick, easy board game in which two players compete to get four pieces in a line.",
     displayUrlText: "4inarow.onrender.com",
     displayUrlHref: "https://4inarow.onrender.com",
     category: ["easy"],
@@ -16,6 +17,9 @@ const game: ServerGame = {
     familyFriendly: true,
     minPlayers: 2,
     maxPlayers: 2,
+
+    guideId: "4inarow",
+    pictures: ["https://i.imgur.com/1Hhs3iQ.png"],
 
     getJoinGameUrl: async () => {
         const newGameUrl = "https://board-game-server.onrender.com/";
