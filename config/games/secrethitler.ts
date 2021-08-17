d084ac778ff328335973e339c19312f0875d853f
Duc Ngo Viet <40763918+duc-talentwunder@users.noreply.github.com>
2021-08-17 16:00:44 +0200

    Update config/games/secrethitler.ts
    Co-authored-by: Peter Oliver <github.com@mavit.org.uk>

--- config/games/secrethitler.ts
+++ config/games/secrethitler.ts
@@ -5,6 +5,12 @@ const game: ServerGame = {
     id: "secrethitler-duc",
     name: "Secret Hitler",
     author: "Duc Ngo Viet",
+    basedOn: {
+        game: "Secret Hitler",
+        author: "Goat, Wolf, & Cabbage LLC",
+        link: "https://www.secrethitler.com/",
+        bggId: 188834,
+    },
     description: `Secret Hitler is a social deduction game for 5-10 people about finding and stopping the Secret Hitler. Players are secretly divided into two teams: the liberals, who have a majority, and the fascists, who are hidden to everyone but each other. If the liberals can learn to trust each other, they have enough votes to control the elections and save the day. But the fascists will say whatever it takes to get elected, advance their agenda, and win the game.`,
     displayUrlText: "secret-hitler.com",
     displayUrlHref: "https://secret-hitler.com/",
